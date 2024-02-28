b975520e34b25bb7b9746d26589fa4a7045888a0
charanmitt <160011864+charanmitt@users.noreply.github.com>
2024-02-28 19:58:52 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+//import { Card } from './components/card';
+import { Player } from './components/player';
+
+function App() {
+  return (
+    <div className='App'>
+      <Player/>
+    </div>
+  )
+}
+
+export default App;
